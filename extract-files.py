f39bec08257821ae0cf6ab68c8fc33415406e9f2
alextrack2013 <alextrack2013@gmail.com>
2025-05-02 18:52:21 +0200

    mt6895-common: Configure Richtap stack
    Change-Id: I6454b240cb5d868a22cf4efabfe0da9f9942c66b

--- extract-files.py
+++ extract-files.py
@@ -54,7 +54,10 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),
     'vendor/lib64/libteei_daemon_vfs.so': blob_fixup()
-        .add_needed('liblog.so')
+        .add_needed('liblog.so'),
+    ('vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service'): blob_fixup()
+        .replace_needed('android.hardware.vibrator-V1-ndk_platform.so', 'android.hardware.vibrator-V1-ndk.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
--- mt6895.mk
+++ mt6895.mk
@@ -323,6 +323,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
+    android.hardware.vibrator-V1-ndk.vendor \
     vibratorfeature-wrapper
 
 # Wi-Fi
