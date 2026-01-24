4a88fc752964f74111e658ae6a553753d90f55d9
Hecheng Yu <kde-yyds@qq.com>
2026-01-24 22:40:13 +0800

    blob_fixup: Link older libtinyxml2
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI incompatible changes which cause runtime crashes.

--- extract-files.py
+++ extract-files.py
@@ -58,6 +58,10 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service'): blob_fixup()
         .replace_needed('android.hardware.vibrator-V1-ndk_platform.so', 'android.hardware.vibrator-V1-ndk.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
+    ('vendor/lib64/hw/mt6895/vendor.mediatek.hardware.pq@2.15-impl.so', 'vendor/lib/hw/mt6895/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    ('vendor/lib64/librt_extamp_intf.so', 'vendor/lib/librt_extamp_intf.so'): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
